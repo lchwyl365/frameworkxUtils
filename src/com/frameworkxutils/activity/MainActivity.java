fce637ac17855ef63aebbf826bf87149b0f8c58c
lchwyl365 <lchwyl365@gmail.com>
2015-08-31 17:35:45 +0800

    first android framework

---
+++ src/com/frameworkxutils/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.frameworkxutils.activity;
+
+public class MainActivity extends BaseActivity
+{
+
+	
+	@Override
+	protected int getLayoutId()
+	{
+		return R.layout.activity_main;
+	}
+
+	@Override
+	protected void initParams()
+	{
+	}
+
+	
+}
